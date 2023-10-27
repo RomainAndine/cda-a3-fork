2282d7ab68f355ff63e1aeccefa2a5fbf93def6b
Adrien <adrisqf@gmail.com>
2023-10-27 15:57:15 +0200

    calculatrice fini

--- adrien.js
+++ adrien.js
@@ -6,25 +6,26 @@ button.addEventListener('click', calcule)
 
 
 function calcule(){
-var result 
-if (isNaN(text1.value) == true || isNaN(text2.value) == true){
-    alert('is not number')
-}else {
-    var num1 = Number(text1.value)
-    var num2 = Number(text2.value)
-    if (comboBox.value == '-'){
-        result = num1 - num2
-    } else if (comboBox.value == '+'){
-        result = num1 + num2
-    }else if (comboBox.value == '/'){
-        result = num1 / num2
-    }else if (comboBox.value == '*'){
-        result = num1 * num2
+    var result 
+    if (isNaN(text1.value) == true || isNaN(text2.value) == true){
+        alert('is not number')
+    }else {
+        var num1 = Number(text1.value)
+        var num2 = Number(text2.value)
+        if (comboBox.value == '-'){
+            result = num1 - num2
+        } else if (comboBox.value == '+'){
+            result = num1 + num2
+        }else if (comboBox.value == '/'){
+            result = num1 / num2
+        }else if (comboBox.value == '*'){
+            result = num1 * num2
+        }
     }
+
+    text1.value =''
+    text2.value =''
+    var p = document.querySelector('p')
+    p.innerHTML =  result
 }
 
-text1.value =''
-text2.value =''
-var p = document.querySelector('p')
-p.innerHTML =  result
-}
