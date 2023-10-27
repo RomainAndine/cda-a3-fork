52fff0fce70c7ce7f4a30045fabce2f93a6b628c
Adrien <adrisqf@gmail.com>
2023-10-27 15:20:42 +0200

    derniere modif calculatrice

--- adrien.js
+++ adrien.js
@@ -7,17 +7,22 @@ button.addEventListener('click', calcule)
 
 function calcule(){
 var result 
-var num1 = Number(text1.value)
-var num2 = Number(text2.value)
-if (comboBox.value == '-'){
-    result = num1 - num2
-} else if (comboBox.value == '+'){
-    result = num1 + num2
-}else if (comboBox.value == '/'){
-    result = num1 / num2
-}else if (comboBox.value == '*'){
-    result = num1 * num2
+if (isNaN(text1.value) == true || isNaN(text2.value) == true){
+    alert('is not number')
+}else {
+    var num1 = Number(text1.value)
+    var num2 = Number(text2.value)
+    if (comboBox.value == '-'){
+        result = num1 - num2
+    } else if (comboBox.value == '+'){
+        result = num1 + num2
+    }else if (comboBox.value == '/'){
+        result = num1 / num2
+    }else if (comboBox.value == '*'){
+        result = num1 * num2
+    }
 }
+
 text1.value =''
 text2.value =''
 var p = document.querySelector('p')
